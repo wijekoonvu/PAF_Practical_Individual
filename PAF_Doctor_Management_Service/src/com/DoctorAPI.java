71f267fcb20a76c1e2a36544d9d256937d1c9506
Udara <Udara@DESKTOP-GIKUIFO>
2020-05-06 15:52:25 +0530

    ..

--- PAF_Doctor_Management_Service/src/com/DoctorAPI.java
+++ PAF_Doctor_Management_Service/src/com/DoctorAPI.java
@@ -26,14 +26,14 @@ public class DoctorAPI extends HttpServlet {
      */
     public DoctorAPI() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
